fe22f7dca14af02a15ba512d0cedf1c846bbce7e
starfrost <mario64crashed@gmail.com>
2022-06-07 20:10:46 +0100

    Initial preparation for arm64 version

---
+++ Lightning2/Rendering/UI/Enums/Alignment.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Lightning2
+{
+    /// <summary>
+    /// Alignment
+    /// 
+    /// June 1, 2022
+    /// 
+    /// Defines text alignment for UIElements
+    /// </summary>
+    public enum Alignment
+    {
+        /// <summary>
+        /// Left-anchored text alignment
+        /// </summary>
+        Left = 0,
+
+        /// <summary>
+        /// Centre-anchored text alignment
+        /// </summary>
+        Centre = 1,
+
+        /// <summary>
+        /// Right-anchored text alignment
+        /// </summary>
+        Right = 2
+    }
+}
