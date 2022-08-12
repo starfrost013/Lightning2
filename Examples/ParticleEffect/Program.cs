1ef4f490ef97dd38d4b8b6680d14f4d4c0b86b82
starfrost <mario64crashed@gmail.com>
2022-08-12 15:56:53 +0100

    delete legacy projects

---
+++ Examples/ParticleEffect/Program.cs
@@ -0,0 +1,27 @@
+﻿using LightningGL; // use lightninggl
+using System.Drawing; // for color
+using System.Numerics;
+
+//Basic Lightning Example (Scene Manager NOT used)
+//©2022 starfrost, August 12, 2022
+
+// Initialise Lightning
+Lightning.Init(args);
+
+Window window = new Window();
+window.Start(new WindowSettings()); // use default windowsettings
+
+// Load a texture
+Texture texture = new Texture();
+
+// Create a particle effect.
+ParticleEffect pe = new ParticleEffect()
+{
+    MaxNumberCreatedEachFrame = 20,
+
+};
+
+{
+    PrimitiveRenderer.DrawText(window, "Basic example", new Vector2(100, 100), Color.White); // no fonts loaded so we use the debug font
+    window.Render();
+}
