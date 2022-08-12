94a8b477121b816d5459ff004f1cc4b09793f31a
starfrost <mario64crashed@gmail.com>
2022-08-12 15:19:09 +0100

    create a visual studio template

---
+++ VSTemplate/Program.cs
@@ -0,0 +1,18 @@
+﻿using LightningGL; // use lightninggl
+using System.Drawing; // for color
+using System.Numerics;
+
+//Basic Lightning Example (Scene Manager NOT used)
+//©2022 starfrost, August 12, 2022
+
+// Initialise Lightning
+Lightning.Init(args);
+
+Window window = new Window();
+window.Start(new WindowSettings()); // use default windowsettings
+
+while (window.Run())
+{
+    PrimitiveRenderer.DrawText(window, "Basic example", new Vector2(100, 100), Color.White); // no fonts loaded so we use the debug font
+    window.Render();
+}
