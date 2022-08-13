42f43b2484c224ffaccfe0d3f144d97eb0101f68
starfrost <mario64crashed@gmail.com>
2022-08-13 15:36:47 +0100

    fix VSTemplate

--- VSTemplate/Program.cs
+++ VSTemplate/Program.cs
@@ -1,19 +1,8 @@
 ﻿using LightningGL; // use lightninggl
-using System.Drawing; // for color
-using System.Numerics;
 
 //Lightning Visual Studio Template
 //©2022 starfrost, August 13, 2022
 
 // Use this to get started
-// Initialise Lightning
+// Initialise Lightning, this will run MainScene
 Lightning.Init(args);
-
-Window window = new Window();
-window.Start(new WindowSettings()); // use default windowsettings
-
-while (window.Run())
-{
-    PrimitiveRenderer.DrawText(window, "Basic example", new Vector2(100, 100), Color.White); // no fonts loaded so we use the debug font
-    window.Render();
-}
