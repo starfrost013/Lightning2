e1f366546696d3ef08fde2f5297bd67fdebc68dc
starfrost <mario64crashed@gmail.com>
2022-08-16 16:06:15 +0100

    bleh

--- Lightning2/Tools/AssemblyInfo.cs
+++ Lightning2/Tools/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.171")]
 [assembly: AssemblyFileVersion("1.0.0.171")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta1 build 171, built 2022-08-16 15:25:22")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta1 build 171, built 2022-08-16 16:05:34")]
 [assembly: AssemblyTitle("Lightning Graphics Library")]
 
 namespace LightningGL
@@ -18,7 +18,7 @@ namespace LightningGL
 		public static int LIGHTNING_VERSION_BUILD = 171;
 		public static string LIGHTNING_VERSION_SPECIFIER = "-beta1";
 		public static string LIGHTNING_VERSION_STRING = "1.0.0-beta1";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.0.0-beta1 build 171, built 2022-08-16 15:25:22";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.0.0-beta1 build 171, built 2022-08-16 16:05:34";
 	}
 }
 
