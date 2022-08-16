59a49d35a3364e7df9aa7cebf1d46a1656d0f364
starfrost <mario64crashed@gmail.com>
2022-08-16 16:09:35 +0100

    add LocalSettings documentation

--- Lightning2/Settings/Local/LocalSettings.cs
+++ Lightning2/Settings/Local/LocalSettings.cs
@@ -17,13 +17,22 @@ namespace LightningGL
         /// </summary>
         public static string Path => GlobalSettings.LocalSettingsPath;
 
+        /// <summary>
+        /// The local settings file - see <see cref="NCINIFile"/>.
+        /// </summary>
         public static NCINIFile LocalSettingsFile { get; private set; }
 
-        public static void Load()
+        /// <summary>
+        /// Loads the Local Settings.
+        /// </summary>
+        internal static void Load()
         {
             LocalSettingsFile = NCINIFile.Parse(Path);
         }
 
+        /// <summary>
+        /// Saves the LocaL sETTINGS.
+        /// </summary>
         public static void Save()
         {
             LocalSettingsFile.Write(Path);
