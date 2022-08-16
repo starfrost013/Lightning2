95c766b36f55ff42e1fd60ca696965084b7875c6
starfrost <mario64crashed@gmail.com>
2022-08-16 16:22:05 +0100

    oosp

--- Lightning2/Tools/AssemblyInfo.cs
+++ Lightning2/Tools/AssemblyInfo.cs
@@ -1,12 +1,11 @@
-// This file defines the version information - generated using the AssemblyInfo.tt T4 template.
+﻿// This file defines the version information - generated using the AssemblyInfo.tt T4 template.
 // Automatically generated file - do not modify
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.170")]
-[assembly: AssemblyFileVersion("1.1.0.170")]
-[assembly: AssemblyInformationalVersion("1.1.0-beta2 build 170, built 2022-08-15 21:03:49")]
-
+[assembly: AssemblyVersion("1.1.0.171")]
+[assembly: AssemblyFileVersion("1.1.0.171")]
+[assembly: AssemblyInformationalVersion("1.1.0-beta2 build 171, built 2022-08-16 16:21:25")]
 [assembly: AssemblyTitle("Lightning Graphics Library")]
 
 namespace LightningGL
@@ -16,9 +15,10 @@ namespace LightningGL
 		public static int LIGHTNING_VERSION_MAJOR = 1;
 		public static int LIGHTNING_VERSION_MINOR = 1; 
 		public static int LIGHTNING_VERSION_REVISION = 0;
-		public static int LIGHTNING_VERSION_BUILD = 170;
+		public static int LIGHTNING_VERSION_BUILD = 171;
 		public static string LIGHTNING_VERSION_SPECIFIER = "-beta2";
 		public static string LIGHTNING_VERSION_STRING = "1.1.0-beta2";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.0-beta2 build 170, built 2022-08-15 21:03:49";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.0-beta2 build 171, built 2022-08-16 16:21:25";
 	}
 }
+
