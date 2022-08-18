83b8800ba75e0b7ee4230aca92b5eb3da083a5a9
starfrost <mario64crashed@gmail.com>
2022-08-18 16:16:18 +0100

    make all other primitiverenderer filled parameters false by default

--- Lightning2/Tools/AssemblyInfo.cs
+++ Lightning2/Tools/AssemblyInfo.cs
@@ -3,9 +3,9 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.172")]
-[assembly: AssemblyFileVersion("1.0.0.172")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta1 build 172, built 2022-08-17 17:04:22")]
+[assembly: AssemblyVersion("1.0.0.173")]
+[assembly: AssemblyFileVersion("1.0.0.173")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta1 build 173, built 2022-08-18 16:11:38")]
 [assembly: AssemblyTitle("Lightning Graphics Library")]
 
 namespace LightningGL
@@ -15,10 +15,10 @@ namespace LightningGL
 		public static int LIGHTNING_VERSION_MAJOR = 1;
 		public static int LIGHTNING_VERSION_MINOR = 0; 
 		public static int LIGHTNING_VERSION_REVISION = 0;
-		public static int LIGHTNING_VERSION_BUILD = 172;
+		public static int LIGHTNING_VERSION_BUILD = 173;
 		public static string LIGHTNING_VERSION_SPECIFIER = "-beta1";
 		public static string LIGHTNING_VERSION_STRING = "1.0.0-beta1";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.0.0-beta1 build 172, built 2022-08-17 17:04:22";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.0.0-beta1 build 173, built 2022-08-18 16:11:38";
 	}
 }
 
