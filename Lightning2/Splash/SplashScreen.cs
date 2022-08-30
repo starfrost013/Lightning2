4898061b9af7b17cebe49b4fce9a28f4d652526d
starfrost <mario64crashed@gmail.com>
2022-08-30 23:56:00 +0100

    begin splashscreen work

---
+++ Lightning2/Splash/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LightningGL
+{
+    /// <summary>
+    /// SplashScreen
+    /// 
+    /// August 30, 2022
+    /// 
+    /// This is essentially a Lightning app running on a separate thread that displays a nice splash screen.
+    /// </summary>
+    internal class SplashScreen
+    {
+    }
+}
