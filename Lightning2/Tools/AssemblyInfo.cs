3950c21ddd163b91da56c05553597ce71d504b4f
starfrost <mario64crashed@gmail.com>
2022-08-31 12:34:19 +0100

    this commit is to change the assemblyinfo to the current build

--- Lightning2/Tools/AssemblyInfo.cs
+++ Lightning2/Tools/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.1.185")]
 [assembly: AssemblyFileVersion("1.0.1.185")]
-[assembly: AssemblyInformationalVersion("1.0.1 build 185, built 2022-08-31 00:13:02")]
+[assembly: AssemblyInformationalVersion("1.0.1 build 185, built 2022-08-31 00:41:40")]
 [assembly: AssemblyTitle("Lightning Graphics Library")]
 
 namespace LightningGL
@@ -18,7 +18,7 @@ namespace LightningGL
 		public static int LIGHTNING_VERSION_BUILD = 185;
 		public static string LIGHTNING_VERSION_SPECIFIER = "";
 		public static string LIGHTNING_VERSION_STRING = "1.0.1";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.0.1 build 185, built 2022-08-31 00:13:02";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.0.1 build 185, built 2022-08-31 00:41:40";
 	}
 }
 
