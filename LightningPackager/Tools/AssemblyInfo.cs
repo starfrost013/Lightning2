17420149f49047e59042dea270de621f5c9194c9
starfrost <mario64crashed@gmail.com>
2022-10-18 13:48:57 +0100

    remove redundant assemblyinfo as all versioning information is in lightningbase

--- LightningPackager/Tools/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file defines the version information - generated using the AssemblyInfo.tt T4 template in LightningBase.
-// Automatically generated file - do not modify
-using System.Reflection;
-
-[assembly: AssemblyVersion("1.1.0.211")]
-[assembly: AssemblyFileVersion("1.1.0.211")]
-[assembly: AssemblyInformationalVersion("1.1.0-alpha build 211, built 2022-09-25 13:56:17")]
-[assembly: AssemblyTitle("Lightning Packaging Library")]
--- NuCore.SDL2-CS/Tools/AssemblyInfo.cs
+++ NuCore.SDL2-CS/Tools/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.1.0.234")]
 [assembly: AssemblyFileVersion("1.1.0.234")]
-[assembly: AssemblyInformationalVersion("1.1.0-alpha build 234, built 2022-10-18 13:26:44")]
+[assembly: AssemblyInformationalVersion("1.1.0-alpha build 234, built 2022-10-18 13:48:15")]
 [assembly: AssemblyTitle("Lightning Base")]
 
 namespace LightningBase
@@ -19,7 +19,7 @@ namespace LightningBase
 		public const string LIGHTNING_VERSION_SPECIFIER = "-alpha";
 		public const string LIGHTNING_VERSION_STRING = "1.1.0-alpha";
 		public const string LIGHTNING_VERSION_BUILD_STRING = "1.1.0.234";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.0-alpha build 234, built 2022-10-18 13:26:44";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.0-alpha build 234, built 2022-10-18 13:48:15";
 	}
 }
 
