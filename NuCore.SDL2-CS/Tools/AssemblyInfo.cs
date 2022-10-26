2a59d3c3bcb2aced2b52bacb138905337b72709b
starfrost <mario64crashed@gmail.com>
2022-10-26 20:59:32 +0100

    remove rc0 tag to prep for final 1.1.0

--- NuCore.SDL2-CS/Tools/AssemblyInfo.cs
+++ NuCore.SDL2-CS/Tools/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.1.0.242")]
 [assembly: AssemblyFileVersion("1.1.0.242")]
-[assembly: AssemblyInformationalVersion("1.1.0-rc0 build 242, built 2022-10-26 20:10:49")]
+[assembly: AssemblyInformationalVersion("1.1.0 build 242, built 2022-10-26 20:15:32")]
 [assembly: AssemblyTitle("Lightning Base")]
 
 namespace LightningBase
@@ -16,10 +16,10 @@ namespace LightningBase
 		public const int LIGHTNING_VERSION_MINOR = 1; 
 		public const int LIGHTNING_VERSION_REVISION = 0;
 		public const int LIGHTNING_VERSION_BUILD = 242;
-		public const string LIGHTNING_VERSION_SPECIFIER = "-rc0";
-		public const string LIGHTNING_VERSION_STRING = "1.1.0-rc0";
+		public const string LIGHTNING_VERSION_SPECIFIER = "";
+		public const string LIGHTNING_VERSION_STRING = "1.1.0";
 		public const string LIGHTNING_VERSION_BUILD_STRING = "1.1.0.242";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.0-rc0 build 242, built 2022-10-26 20:10:49";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.0 build 242, built 2022-10-26 20:15:32";
 	}
 }
 
