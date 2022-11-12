0b8ca32cba7b7f385625343d72c62e2ffbe87065
starfrost <mario64crashed@gmail.com>
2022-11-12 20:50:32 +0000

    begin work on 1.1.4

--- Lightning2/Rendering/UI/UIManager.cs
+++ Lightning2/Rendering/UI/UIManager.cs
@@ -15,15 +15,16 @@
         /// <param name="gadget">The <see cref=""/></param>
         public override Gadget AddAsset(Renderer cRenderer, Gadget gadget)
         {
-            NCLogging.Log($"Creating new Gadget::{gadget.GetType().Name}");
+            NCLogging.Log($"Creating new Gadget {gadget.GetType().Name}...");
             Assets.Add(gadget);
             return gadget;
         }
 
         public override void RemoveAsset(Renderer cRenderer, Gadget gadget)
         {
-            if (!Assets.Contains(gadget)) _ = new NCException($"Attempted to remove a gadget of type ({gadget.GetType().Name} that is not in the UI Manager - you must add it first!", 135, "Called UIManager::RemoveElement with a gadget property that does not correspond to a Gadget loaded by the UI Manager!", NCExceptionSeverity.Warning);
-            NCLogging.Log($"Removing a Gadget::{gadget.GetType().Name} from UIManager");
+            if (!Assets.Contains(gadget)) _ = new NCException($"Attempted to remove a gadget of type ({gadget.GetType().Name} that is not in the UI Manager - you must add it first!", 
+                135, "Called UIManager::RemoveElement with a gadget property that does not correspond to a Gadget loaded by the UI Manager!", NCExceptionSeverity.Warning);
+            NCLogging.Log($"Removing a Gadget {gadget.GetType().Name}...");
             Assets.Remove(gadget);
         }
 
@@ -35,7 +36,8 @@
         {
             foreach (Gadget uiElement in Assets)
             {
-                if (uiElement.Size == default) _ = new NCException($"Attempted to draw a gadget with no size, you will not see it!", 122, "Gadget::Size = (0,0)!", NCExceptionSeverity.Warning, null, true);
+                if (uiElement.Size == default) _ = new NCException($"Attempted to draw a gadget with no size, you will not see it!", 122, "Gadget::Size = (0,0)!", 
+                    NCExceptionSeverity.Warning, null, true);
                 if (uiElement.OnRender != null)
                 {
                     uiElement.OnRender(cRenderer);
--- NuCore.SDL2-CS/Tools/AssemblyInfo.cs
+++ NuCore.SDL2-CS/Tools/AssemblyInfo.cs
@@ -3,9 +3,9 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.3.255")]
-[assembly: AssemblyFileVersion("1.1.3.255")]
-[assembly: AssemblyInformationalVersion("1.1.3 build 255, built 2022-11-08 12:51:51")]
+[assembly: AssemblyVersion("1.1.4.259")]
+[assembly: AssemblyFileVersion("1.1.4.259")]
+[assembly: AssemblyInformationalVersion("1.1.4 build 259, built 2022-11-12 18:30:40")]
 [assembly: AssemblyTitle("Lightning Base")]
 
 namespace LightningBase
@@ -14,12 +14,12 @@ namespace LightningBase
 	{
 		public const int LIGHTNING_VERSION_MAJOR = 1;
 		public const int LIGHTNING_VERSION_MINOR = 1; 
-		public const int LIGHTNING_VERSION_REVISION = 3;
-		public const int LIGHTNING_VERSION_BUILD = 255;
+		public const int LIGHTNING_VERSION_REVISION = 4;
+		public const int LIGHTNING_VERSION_BUILD = 259;
 		public const string LIGHTNING_VERSION_SPECIFIER = "";
-		public const string LIGHTNING_VERSION_STRING = "1.1.3";
-		public const string LIGHTNING_VERSION_BUILD_STRING = "1.1.3.255";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.3 build 255, built 2022-11-08 12:51:51";
+		public const string LIGHTNING_VERSION_STRING = "1.1.4";
+		public const string LIGHTNING_VERSION_BUILD_STRING = "1.1.4.259";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "1.1.4 build 259, built 2022-11-12 18:30:40";
 	}
 }
 
