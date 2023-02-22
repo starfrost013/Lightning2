6b7af321a91ef5ab2565f6815b9c012d4b8b8e1b
Xena BEAT <mario64crashed@gmail.com>
2023-02-22 15:07:23 +0000

    fix compile

--- NuCore.SDL2-CS/Tools/AssemblyInfo.cs
+++ NuCore.SDL2-CS/Tools/AssemblyInfo.cs
@@ -3,9 +3,9 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.360")]
-[assembly: AssemblyFileVersion("2.0.0.360")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha build 360, built 2023-02-21 21:35:21")]
+[assembly: AssemblyVersion("2.0.0.361")]
+[assembly: AssemblyFileVersion("2.0.0.361")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha build 361, built 2023-02-22 15:02:18")]
 [assembly: AssemblyTitle("Lightning Base")]
 
 namespace LightningBase
@@ -15,11 +15,11 @@ namespace LightningBase
 		public const int LIGHTNING_VERSION_MAJOR = 2;
 		public const int LIGHTNING_VERSION_MINOR = 0; 
 		public const int LIGHTNING_VERSION_REVISION = 0;
-		public const int LIGHTNING_VERSION_BUILD = 360;
+		public const int LIGHTNING_VERSION_BUILD = 361;
 		public const string LIGHTNING_VERSION_SPECIFIER = "-alpha";
 		public const string LIGHTNING_VERSION_STRING = "2.0.0-alpha";
-		public const string LIGHTNING_VERSION_BUILD_STRING = "2.0.0.360";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "2.0.0-alpha build 360, built 2023-02-21 21:35:21";
+		public const string LIGHTNING_VERSION_BUILD_STRING = "2.0.0.361";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "2.0.0-alpha build 361, built 2023-02-22 15:02:18";
 	}
 }
 
