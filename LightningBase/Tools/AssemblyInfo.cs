c3261fb568ca7619475cfa2ffc64aec5a4e3d796
starfrost013 <mario64crashed@gmail.com>
2023-03-18 18:11:14 +0000

    fix compilation

--- LightningBase/Tools/AssemblyInfo.cs
+++ LightningBase/Tools/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// This file defines the version information - generated using the AssemblyInfo.tt T4 template in LightningBase.
 // Automatically generated file - do not modify
 
-[assembly: AssemblyVersion("2.0.0.384")]
-[assembly: AssemblyFileVersion("2.0.0.384")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta build 384, built 2023-03-17 23:09:02")]
+[assembly: AssemblyVersion("2.5.0.385")]
+[assembly: AssemblyFileVersion("2.5.0.385")]
+[assembly: AssemblyInformationalVersion("2.5.0-alpha build 385, built 2023-03-18 18:10:39")]
 [assembly: AssemblyTitle("The Lightning Game Engine")]
 
 namespace LightningBase
@@ -11,13 +11,13 @@ namespace LightningBase
 	public static class LightningVersion
 	{
 		public const int LIGHTNING_VERSION_MAJOR = 2;
-		public const int LIGHTNING_VERSION_MINOR = 0; 
+		public const int LIGHTNING_VERSION_MINOR = 5; 
 		public const int LIGHTNING_VERSION_REVISION = 0;
-		public const int LIGHTNING_VERSION_BUILD = 384;
-		public const string LIGHTNING_VERSION_SPECIFIER = "-beta";
-		public const string LIGHTNING_VERSION_STRING = "2.0.0-beta";
-		public const string LIGHTNING_VERSION_BUILD_STRING = "2.0.0.384";
-		public static string LIGHTNING_VERSION_EXTENDED_STRING = "2.0.0-beta build 384, built 2023-03-17 23:09:02";
+		public const int LIGHTNING_VERSION_BUILD = 385;
+		public const string LIGHTNING_VERSION_SPECIFIER = "-alpha";
+		public const string LIGHTNING_VERSION_STRING = "2.5.0-alpha";
+		public const string LIGHTNING_VERSION_BUILD_STRING = "2.5.0.385";
+		public static string LIGHTNING_VERSION_EXTENDED_STRING = "2.5.0-alpha build 385, built 2023-03-18 18:10:39";
 	}
 }
 
