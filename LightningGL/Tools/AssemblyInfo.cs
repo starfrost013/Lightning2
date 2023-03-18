36d0a6c66fdbe49367f22d36bfdea2a903ebb6fa
starfrost013 <mario64crashed@gmail.com>
2023-03-18 20:10:41 +0000

    more renaming, update versions to 2.5

--- LightningGL/Tools/AssemblyInfo.cs
+++ LightningGL/Tools/AssemblyInfo.cs
@@ -1,3 +1,3 @@
-﻿[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
+﻿[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0.0")]
 [assembly: AssemblyTitle("The Lightning Game Engine - Rendering")]
--- LightningPackager/Tools/AssemblyInfo.cs
+++ LightningPackager/Tools/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0.0")]
 [assembly: AssemblyTitle("The Lightning Game Engine - Packaging")]
