e3f8c9bbcd2954685a786b52ec9d9877c93b16bd
starfrost013 <mario64crashed@gmail.com>
2023-03-21 16:09:14 +0000

    ellipse doesn't need off by one fix

--- Lightning2/Rendering/Primitives/Ellipse.cs
+++ Lightning2/Rendering/Primitives/Ellipse.cs
@@ -19,11 +19,11 @@
             if (BorderSize.X > 0
                 && BorderSize.Y > 0)
             {
-                Lightning.Renderer.DrawEllipse((int)RenderPosition.X - ((int)BorderSize.X + 1), (int)RenderPosition.Y - ((int)BorderSize.X + 1),
-                    (int)Size.X + ((int)BorderSize.X - 1), (int)Size.Y + ((int)BorderSize.Y - 1), BorderColor.R, BorderColor.G, BorderColor.B, BorderColor.A, true);
+                Lightning.Renderer.DrawEllipse((int)RenderPosition.X - (int)BorderSize.X, (int)RenderPosition.Y - (int)BorderSize.X,
+                    (int)Size.X + (int)BorderSize.X, (int)Size.Y + (int)BorderSize.Y, BorderColor.R, BorderColor.G, BorderColor.B, BorderColor.A, true);
             }
 
-            Lightning.Renderer.DrawEllipse((int)RenderPosition.X, (int)RenderPosition.Y, Size.X, (int)Size.Y, Color.R, Color.G, Color.B, Color.A, Filled);
+            Lightning.Renderer.DrawEllipse((int)RenderPosition.X, (int)RenderPosition.Y, (int)Size.X, (int)Size.Y, Color.R, Color.G, Color.B, Color.A, Filled);
         }
     }
 }
