fc84e8ffcb980341efc12bdb970d082887995766
starfrost013 <mario64crashed@gmail.com>
2023-03-21 19:30:02 +0000

    port fontandtext

--- Examples/FontAndText/Program.cs
+++ Examples/FontAndText/Program.cs
@@ -1,34 +1,6 @@
-﻿using LightningGL; // use lightninggl
-using static LightningGL.Lightning;
-using System.Drawing; // for color
-using System.Numerics;
-
-//Font and text example
-//©2022 starfrost, August 12, 2022
+﻿// Font and text example
+// © 2022-2023 starfrost, March 21, 2023
 
 // Initialise Lightning
-Init(args);
+InitClient();
 
-Renderer renderer = new Renderer();
-renderer.Start(new RendererSettings()); // use default Renderersettings
-
-// if you do not provide a path it will load from the system font directory
-// TrueType fonts only, you don't need to load bold/italic/etc fonts - drawtext has parameters for this
-FontManager.LoadFont("Arial", 24, "Arial.24pt");
-FontManager.LoadFont("comic", 24, "ComicSans.24pt"); // comic.ttf is the file name for comic sans ms in Renderers
-FontManager.LoadFont("segoeui", 24, "SegoeUI.24pt"); //v segoeui.ttf is the filename for segoe ui
-FontManager.LoadFont("consola", 18, "Consolas.18pt"); // same for consolas...
-FontManager.LoadFont("consola", 36, "Consolas.36pt");
-
-while (renderer.Run())
-{
-    // backgrounds etc supported
-    PrimitiveRenderer.DrawText(renderer, "Font test example (primitive debug font)", new Vector2(100, 100), Color.White); // no fonts loaded so we use the debug font
-    TextManager.DrawText(renderer, "Arial 24pt", "Arial.24pt", new Vector2(100, 150), Color.White, Color.Red, LightningBase.SDL_ttf.TTF_FontStyle.Italic); // Arial.24pt, italic
-    TextManager.DrawText(renderer, "Comic Sans 24pt", "ComicSans.24pt", new Vector2(100, 200), Color.Blue);
-    TextManager.DrawText(renderer, "Segoe UI 24pt", "SegoeUI.24pt", new Vector2(100, 250), Color.White, Color.Yellow, LightningBase.SDL_ttf.TTF_FontStyle.Underline | LightningBase.SDL_ttf.TTF_FontStyle.Bold);
-    TextManager.DrawText(renderer, "Consolas 18pt", "Consolas.18pt", new Vector2(100, 300), Color.White, Color.Red);
-    TextManager.DrawText(renderer, "Consolas 36pt\nMultiline text!", "Consolas.36pt", new Vector2(100, 350), Color.White, Color.Red);
-
-    renderer.Render();
-}
--- Examples/FontAndText/Scenes/MainScene.cs
+++ Examples/FontAndText/Scenes/MainScene.cs
@@ -20,7 +20,21 @@ namespace BasicScene
 
         public override void SwitchTo(Scene? oldScene)
         {
-            
+
+            // if you do not provide a path it will load from the system font directory
+            // TrueType fonts only, you don't need to load bold/italic/etc fonts - drawtext has parameters for this
+            Lightning.Renderer.AddRenderable(new Font("Arial.ttf", 24, "Arial.24pt"));
+            Lightning.Renderer.AddRenderable(new Font("comic.ttf", 24, "ComicSans.24pt")); // comic.ttf is the file name for comic sans ms in Renderers
+            Lightning.Renderer.AddRenderable(new Font("segoeui.ttf", 24, "SegoeUI.24pt")); //v segoeui.ttf is the filename for segoe ui
+            Lightning.Renderer.AddRenderable(new Font("consola.ttf", 18, "Consolas.18pt")); // same for consolas...
+            Lightning.Renderer.AddRenderable(new Font("consola.ttf", 36, "Consolas.36pt"));
+
+            // backgrounds etc supported
+            Lightning.Renderer.AddRenderable(new TextBlock("Text1", "Arial 24pt", "Arial.24pt", new Vector2(100, 150), Color.White, Color.Red)); // Arial.24pt, italic
+            Lightning.Renderer.AddRenderable(new TextBlock("Text2", "Comic Sans 24pt", "ComicSans.24pt", new Vector2(100, 200), Color.Blue));
+            Lightning.Renderer.AddRenderable(new TextBlock("Text3", "Segoe UI 24pt", "SegoeUI.24pt", new Vector2(100, 250), Color.White, Color.Yellow));
+            Lightning.Renderer.AddRenderable(new TextBlock("Text4", "Consolas 18pt", "Consolas.18pt", new Vector2(100, 300), Color.White, Color.Red));
+            Lightning.Renderer.AddRenderable(new TextBlock("Text5", "Consolas 36pt\nMultiline text!", "Consolas.36pt", new Vector2(100, 350), Color.White, Color.Red));
         }
 
         public override void SwitchFrom(Scene newScene)
@@ -30,7 +44,7 @@ namespace BasicScene
 
         public override void Render()
         {
-            Lightning.Renderer.AddRenderable(new TextBlock("Scene1", "Hello from MainScene", "DebugFont", new Vector2(300, 300), Color.Red));
+
         }
     }
 }
